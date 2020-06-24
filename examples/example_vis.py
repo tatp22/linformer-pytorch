e5352fc53e44f76829f81d3b7bde18b5d68b604c
Peter Tatkowski <tatp22@gmail.com>
2020-06-25 01:47:23 +0200

    Changed the vis file

--- examples/example_vis.py
+++ examples/example_vis.py
@@ -20,8 +20,8 @@ x = torch.randn(1, 512, 16)
 y = model(x, visualize=True)
 vis = Visualizer(model)
 vis.plot_all_heads(title="All P_bar matrices",
-                   show=True,
-                   save_file=None,
-                   figsize=(30,20),
+                   show=False,
+                   save_file="../head_vis.png",
+                   figsize=(8,6),
                    n_limit=256)
 print(y) # (1, 512, 16)
