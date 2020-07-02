6152c841f9d8708785dc279465724a5c4d176618
Peter Tatkowski <tatp22@gmail.com>
2020-07-02 22:39:12 +0200

    Fixed import

--- linformer_pytorch/__init__.py
+++ linformer_pytorch/__init__.py
@@ -1,3 +1,3 @@
-from linformer_pytorch.linformer_pytorch import Linformer, LinformerLM, MHAttention, LinearAttentionHead, FeedForward, PositionalEmbedding, get_linear
+from linformer_pytorch.linformer_pytorch import Linformer, LinformerLM, MHAttention, LinearAttentionHead, FeedForward, PositionalEmbedding, get_EF
 from linformer_pytorch.padder import Padder
 from linformer_pytorch.visualizer import Visualizer
